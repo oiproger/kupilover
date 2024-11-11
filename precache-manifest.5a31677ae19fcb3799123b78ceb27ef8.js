9f50a6c64e0d66699bcb5c0b0a60bfc5765d3831
oiproger <reiman.daniil@gmail.com>
2024-11-11 14:06:10 +0700

    Updates

---
+++ precache-manifest.5a31677ae19fcb3799123b78ceb27ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a032c1495013b811f952d34108a24474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f8719180c3d9e497560",
+    "url": "/static/css/main.4dae9c8c.chunk.css"
+  },
+  {
+    "revision": "69444f5dc5a964996930",
+    "url": "/static/js/2.99f2f579.chunk.js"
+  },
+  {
+    "revision": "0f8719180c3d9e497560",
+    "url": "/static/js/main.ff9bfa7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
