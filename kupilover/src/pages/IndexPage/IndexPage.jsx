36e933d57af32a58a62678ef580eb73b02923736
oiproger <reiman.daniil@gmail.com>
2024-12-17 11:40:59 +0700

    newDatas

--- kupilover/src/pages/IndexPage/IndexPage.jsx
+++ kupilover/src/pages/IndexPage/IndexPage.jsx
@@ -1,16 +1,9 @@
-import { Section, Cell, Image, List, Button, Modal, Placeholder, Select, Caption, Tabbar, Card, Textarea } from '@telegram-apps/telegram-ui';
-import React, { useState } from 'react';
-import { popup } from '@telegram-apps/sdk';
+import { Section, List, Button, Placeholder, } from '@telegram-apps/telegram-ui';
+import React from 'react';
 
 import { Link } from '@/components/Link/Link.jsx';
 
-import tonSvg from './ton.svg';
-
 import './IndexPage.css';
-import { SectionHeader } from '@telegram-apps/telegram-ui/dist/components/Blocks/Section/components/SectionHeader/SectionHeader';
-import { ModalHeader } from '@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader';
-import { CardChip } from '@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardChip/CardChip';
-import { CardCell } from '@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardCell/CardCell';
 
 /**
  * @returns {JSX.Element}
